a9570920e222ac314403010ab8f02512dac24ba0
yenfharm <yenfhael.arm@gmail.com>
2024-02-27 15:29:56 -0400

    prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        
+        Console.Write("What is your first name?: ");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name?: ");
+        string second = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {second}, {first} {second}");
     }
 }
