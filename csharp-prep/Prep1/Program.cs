49ca34367046f55c9ed2fcef98d81cf61d02b0d2
yenfharm <yenfhael.arm@gmail.com>
2024-02-27 15:50:20 -0400

    changes prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,11 +6,11 @@ class Program
     {
         Console.WriteLine("Hello Prep1 World!");
         
-        Console.Write("What is your first name?: ");
+        Console.Write("What is your first name? ");
         string first = Console.ReadLine();
-        Console.Write("What is your last name?: ");
-        string second = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {second}, {first} {second}.");
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
