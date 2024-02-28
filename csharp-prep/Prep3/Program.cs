114b78b6b49cb89bef84897837024cc7396f07d3
yenfharm <yenfhael.arm@gmail.com>
2024-02-28 13:30:49 -0400

    changes prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number?: ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(0,100);
+        
+        int myGuess = 0;
+
+        while (randomNumber != myGuess)
+        {
+
+            Console.Write("What is your guess?: ");
+            myGuess = int.Parse(Console.ReadLine());
+            
+            if (randomNumber < myGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (randomNumber > myGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it");
+            }
+        }  
     }
+        
+        
 }
