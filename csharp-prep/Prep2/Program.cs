523b0a3f9dc1e2396cbede62492abe10b3ff4acb
yenfharm <yenfhael.arm@gmail.com>
2024-02-27 21:44:43 -0400

    changes prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string percentage = Console.ReadLine();
+
+        int x = int.Parse(percentage);
+
+        string letter = "";
+
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter is {letter}");
+
+        if (x >= 70)
+        {
+            Console.WriteLine("--> Great, you passed the course <--");
+        }
+        else 
+        {
+            Console.WriteLine("--> Don't give up, you can do it better next time <--");
+        }
+
     }
 }
