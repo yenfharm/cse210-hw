d2d9e1b7cd610ee5d1797fe0a1359e0519a6f766
yenfharm <yenfhael.arm@gmail.com>
2024-03-05 00:00:20 -0400

    new

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Development";
+        job1._company = "Google";
+        job1._startYear = 2013;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Designer";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Yenfhael Armas";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
     }
 }
