d913b0b3b5c67d1b53c74b80137bbc19c6dc9980
yenfharm <yenfhael.arm@gmail.com>
2024-03-05 00:13:47 -0400

    changes

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -4,7 +4,6 @@ public class Resume
 {
     public string _name;
 
-    // Make sure to initialize your list to a new List before you use it.
     public List<Job> _jobs = new List<Job>();
 
     public void Display()
@@ -12,10 +11,8 @@ public class Resume
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs:");
 
-        // Notice the use of the custom data type "Job" in this loop
         foreach (Job job in _jobs)
         {
-            // This calls the Display method on each job
             job.Display();
         }
     }
