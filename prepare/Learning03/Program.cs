e70b9bae70cba80495969550047a9ee4d81a160e
yenfharm <yenfhael.arm@gmail.com>
2024-03-12 11:46:23 -0400

    new

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fr1 = new Fraction();
+        Console.WriteLine(fr1.GetFractionString());
+        Console.WriteLine(fr1.GetDecimalValue());
+
+        Fraction fr2 = new Fraction(5);
+        Console.WriteLine(fr2.GetFractionString());
+        Console.WriteLine(fr2.GetDecimalValue());
+
+        Fraction fr3 = new Fraction(3, 4);
+        Console.WriteLine(fr3.GetFractionString());
+        Console.WriteLine(fr3.GetDecimalValue());
+
+        Fraction fr4 = new Fraction(1, 3);
+        Console.WriteLine(fr4.GetFractionString());
+        Console.WriteLine(fr4.GetDecimalValue());
     }
 }
